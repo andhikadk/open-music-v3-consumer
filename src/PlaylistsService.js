3d59c81729582789957581643b0270b93971e539
Andhika Dwi Khalisyahputra <andhikadwi980@gmail.com>
2023-08-24 05:38:01 +0700

    refactor: clean query sql

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -7,25 +7,20 @@ class PlaylistsService {
 
   async getPlaylistById(id) {
     const { rows } = await this._pool.query({
-      text: `SELECT playlists.* FROM playlists
-      INNER JOIN users ON playlists.owner = users.id WHERE playlists.id = $1`,
+      text: `SELECT p.id, p.name FROM playlists p
+      INNER JOIN users u ON p.owner = u.id WHERE p.id = $1`,
       values: [id],
     });
-
-    return rows.map((row) => ({
-      id: row.id,
-      name: row.name,
-      username: row.username,
-    }))[0];
+    return rows[0];
   }
 
   async getPlaylist(playlistId) {
     const playlist = await this.getPlaylistById(playlistId);
 
     const { rows } = await this._pool.query({
-      text: `SELECT songs.id, songs.title, songs.performer FROM songs
-      INNER JOIN playlist_songs ON songs.id = playlist_songs.song_id
-      WHERE playlist_songs.playlist_id = $1`,
+      text: `SELECT s.id, s.title, s.performer FROM songs s
+      INNER JOIN playlist_songs ps ON s.id = ps.song_id
+      WHERE ps.playlist_id = $1`,
       values: [playlistId],
     });
 
